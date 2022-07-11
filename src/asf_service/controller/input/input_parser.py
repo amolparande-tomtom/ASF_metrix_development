70a6b99b505fec3d951189aab2e607f59f90cf65
potnis <isha.potnis@tomtom.com>
2022-07-11 16:10:54 +0530

    updated input params

--- src/asf_service/controller/input/input_parser.py
+++ src/asf_service/controller/input/input_parser.py
@@ -3,7 +3,6 @@
 Parsing all inputs for the web service
 -----------------------------
 """
-
 from flask_restx import reqparse
 
 from src.asf_service.Commons.mnrserver import MnrServer
@@ -14,6 +13,8 @@ parser.add_argument('mnr_db_url', required=True, help='Select your MNR DB url',
 parser.add_argument('mnr_schema', required=True, help='Enter your mnr schema name',
                     choices=("eur_cas", "nam", "lam_mea_oce_sea", "ind_ind", "isr_isr", "s_o_s_o", "indoor"))
 parser.add_argument('vad_schema', required=True, help='Enter your vad schema name')
-parser.add_argument('input_file_path', required=True, help='Enter your input file Path')
-parser.add_argument('output_directory_path', required=True, help='Enter your output directory Path')
-parser.add_argument('language_codes', type="Array", help='Enter languages', required=True)
+parser.add_argument('input_file_path',
+                    required=True, help='Enter your input file Path')
+parser.add_argument('output_directory_path',
+                    required=True, help='Enter your output file Path')
+parser.add_argument('language_codes', action="append", help='Enter languages', required=True)
